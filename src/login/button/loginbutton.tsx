7599ef2318e8fbb3eed2e63eeab3081a4afb15e6
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-03-18 20:18:04 +0800

    loginbutton.tsx

---
+++ src/login/button/loginbutton.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { Box, Typography, styled } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const LoginButton = styled(Button)({
+  fontSize: 16,
+  padding: "6px 12px",
+  backgroundColor: "#000",
+  borderRadius: 10,
+  "&:hover": {
+    backgroundColor: "#000",
+  },
+  "&:active": {
+    backgroundColor: "#000",
+  },
+});
+
+const RegisterButton = styled(Button)({
+  fontSize: 16,
+  padding: "6px 12px",
+  border: "1.6px solid #000",
+  borderRadius: 10,
+  color: "#000",
+  "&:hover": {
+    border: "1.6px solid #000",
+    backgroundColor: "transparent",
+  },
+  "&:active": {
+    border: "1.6px solid #000",
+  },
+  "&:focus": {
+    border: "1.6px solid #000",
+  },
+});
+
+export default function LoginButtons() {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" width="100%">
+      <LoginButton variant="contained" fullWidth disableRipple>
+        登录
+      </LoginButton>
+
+      <Typography
+        variant="body1"
+        style={{
+          fontSize: "16px",
+          color: "#000",
+        }}
+      >
+        or
+      </Typography>
+
+      <RegisterButton variant="outlined" fullWidth disableRipple>
+        注册
+      </RegisterButton>
+    </Box>
+  );
+}
