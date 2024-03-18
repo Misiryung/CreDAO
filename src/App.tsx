a862617dc5df28487af26060a2446ba312a9f8ff
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-03-18 20:23:29 +0800

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import VideoDetail from "./video/components/video/detail";
 import Feed from "./video/components/feed/feed";
-import LoginPage from "./login/home";
+import LoginHome from "./login/loginhome";
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#FFF" }}>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LoginHome />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/search/:searchTerm" element={<Feed />} />
       </Routes>
