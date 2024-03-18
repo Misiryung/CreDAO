c2e8186891439b98ee6fef6b580d3a6f9dcc60aa
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-03-18 20:19:52 +0800

    page1.tsx

--- src/login/page/page1.tsx
+++ src/login/page/page1.tsx
@@ -1,89 +1,71 @@
 import React from "react";
-import { Paper, Box, Typography } from "@mui/material";
-import AccountTextField from "../components/account";
-import PasswordTextField from "../components/password";
+import { Box, Typography } from "@mui/material";
+import AccountTextFields from ".././textfield/accounttextfield";
+import PasswordTextFields from ".././textfield/passwordtextfield";
 import SelectBox from "../components/selectbox";
-import LoginButtons from "../components/loginbutton";
+import LoginButtons from ".././button/loginbutton";
 
 const Page1: React.FC = () => {
   return (
-    <Paper
+    <Box
       sx={{
-        width: "25vw",
-        height: "100vh",
-        bgcolor: "#FFF",
-        boxShadow: "none",
-        borderRadius: 0,
-        border: "none",
-        position: "relative",
+        width: "100%",
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
         alignItems: "center",
       }}
     >
       <Box
         sx={{
-          width: "80%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h1"
+          style={{
+            fontWeight: "bold",
+            fontSize: "32px",
+            color: "#000",
+            textAlign: "left",
+            marginRight: "auto",
+          }}
+        >
+          欢迎回来
+        </Typography>
+        <Typography
+          variant="body1"
+          style={{
+            fontSize: "16px",
+            color: "#7F7F7F",
+            textAlign: "right",
+            marginLeft: "auto",
+          }}
+        >
+          验证码登录
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          position: "absolute",
-          top: "16%",
+          marginTop: "80px",
+          gap: "15px",
         }}
       >
+        <AccountTextFields />
+        <PasswordTextFields />
         <Box
           sx={{
             width: "100%",
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
-          }}
-        >
-          <Typography
-            variant="h1"
-            style={{
-              fontWeight: "bold",
-              fontSize: "32px",
-              color: "#000",
-              textAlign: "left",
-              marginRight: "auto",
-            }}
-          >
-            欢迎回来
-          </Typography>
-          <Typography
-            variant="body1"
-            style={{
-              fontSize: "16px",
-              color: "#7F7F7F",
-              textAlign: "right",
-              marginLeft: "auto",
-            }}
-          >
-            验证码登录
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            marginTop: "80px",
-            gap: "15px",
-          }}
-        >
-          <AccountTextField />
-          <PasswordTextField />
-        </Box>
-        <Box
-          sx={{
-            width: "100%",
-            marginTop: "15px",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center", // 添加此行以在垂直方向上居中
+            justifyContent: "center",
           }}
         >
           <SelectBox />
@@ -99,20 +81,19 @@ const Page1: React.FC = () => {
             忘记密码
           </Typography>
         </Box>
-        <Box
-          sx={{
-            width: "100%",
-            marginTop: "80px",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center", // 添加此行以在垂直方向上居中
-          }}
-        >
-          <LoginButtons />
-        </Box>
       </Box>
-    </Paper>
+      <Box
+        sx={{
+          width: "100%",
+          marginTop: "80px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <LoginButtons />
+      </Box>
+    </Box>
   );
 };
 
