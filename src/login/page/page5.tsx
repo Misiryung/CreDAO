2e4b9aef2b3a1c16770ee119b841710ab9a79ead
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-03-19 16:02:17 +0800

    page5.tsx

---
+++ src/login/page/page5.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import ImportPrivatekey from "../components/importprivatekey";
+import CreatedButtons from "../button/createdbutton";
+
+const Page5: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        variant="h1"
+        style={{
+          fontWeight: "bold",
+          fontSize: "32px",
+          color: "#000",
+          textAlign: "left",
+          marginRight: "auto",
+        }}
+      >
+        确认助记词
+      </Typography>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "50px",
+          gap: "5px",
+        }}
+      >
+        <Typography
+          variant="body1"
+          style={{
+            fontSize: "18px",
+            color: "#000",
+            textAlign: "left",
+            marginRight: "auto",
+          }}
+        >
+          请在私密环境中完成此步骤
+        </Typography>
+        <Typography
+          variant="body1"
+          style={{
+            fontSize: "16px",
+            color: "#FF0000",
+            textAlign: "left",
+            marginRight: "auto",
+          }}
+        >
+          私钥、助记词一旦泄露，资产即可被他人掌控
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+          marginTop: "50px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <ImportPrivatekey />
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+          marginTop: "80px",
+        }}
+      >
+        <CreatedButtons />
+      </Box>
+    </Box>
+  );
+};
+
+export default Page5;
