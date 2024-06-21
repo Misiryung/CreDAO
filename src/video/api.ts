c1f2dee0d59af4f968d00a8f3124aa84478be7ef
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-06-21 19:53:57 +0800

    Update api.ts

--- src/video/api.ts
+++ src/video/api.ts
@@ -7,7 +7,7 @@ const API_KEY = "AIzaSyCsZ2YacYLolQeEBSf_m87mTg5A7xT1o-0";
 export const searchVideos = async (query: string) => {
   try {
     const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${API_KEY}`,
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${API_KEY}&maxResults=1`
     );
     return response.data.items;
   } catch (error) {
@@ -19,7 +19,7 @@ export const searchVideos = async (query: string) => {
 export const fetchVideoInfo = async (videoId: string) => {
   try {
     const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`,
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}&maxResults=1`
     );
     return response.data.items[0];
   } catch (error) {
@@ -31,7 +31,7 @@ export const fetchVideoInfo = async (videoId: string) => {
 export const fetchChannelInfo = async (channelId: string) => {
   try {
     const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${API_KEY}`,
+      `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${API_KEY}&maxResults=1`
     );
     return response.data.items[0].snippet;
   } catch (error) {
