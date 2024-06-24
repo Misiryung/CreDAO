6c138e78987bfb770c453bdfbe3c3191fcaa2cbc
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-06-24 20:15:36 +0800

    Update detail.tsx

--- src/video/components/video/detail.tsx
+++ src/video/components/video/detail.tsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import { fetchVideoInfo } from "../../api";
-import Loader from "../feed/feed";
+import { fetchVideoInfo, fetchChannelInfo } from "../../api";
+import Sidebar from "../sidebar/sidebar";
+import Navbar from "../navbar/navbar";
 
 const VideoDetail: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string>("New");
   const [videoDetail, setVideoDetail] = useState(null);
+  const [channelAvatar, setChannelAvatar] = useState<string>("");
+  const [channelTitle, setChannelTitle] = useState<string>("");
+  const [subscriberCount, setSubscriberCount] = useState<number | undefined>(0);
   const { id } = useParams<{ id: string }>() ?? { id: "" };
 
   useEffect(() => {
@@ -16,6 +21,17 @@ const VideoDetail: React.FC = () => {
       try {
         const videoDetailData = await fetchVideoInfo(id);
         setVideoDetail(videoDetailData);
+
+        if (videoDetailData?.snippet?.channelId) {
+          const channelInfo = await fetchChannelInfo(
+            videoDetailData.snippet.channelId
+          );
+          if (channelInfo) {
+            setChannelAvatar(channelInfo.thumbnails.default.url);
+            setChannelTitle(channelInfo.title);
+            setSubscriberCount(channelInfo.subscriberCount);
+          }
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -24,27 +40,61 @@ const VideoDetail: React.FC = () => {
     fetchData();
   }, [id]);
 
-  if (!videoDetail) return <Loader />;
-
-  const {
-    snippet: { title, channelId, channelTitle },
-  } = videoDetail;
+  const { snippet } = videoDetail || {
+    snippet: { title: "", channelId: "", channelTitle: "" },
+  };
+  const { title, channelId } = snippet;
 
   return (
-    <Box height="92vh">
-      <Stack direction={{ xs: "column", md: "row" }}>
-        <Box flex={1}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <Box
+        sx={{
+          width: "80px",
+          height: "100%",
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Navbar />
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
           <Box
             sx={{
-              width: "calc(100% - 20px)",
-              margin: "auto",
-              marginTop: "10px",
-              paddingLeft: "20px",
+              flex: 1,
+              padding: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
             }}
           >
             <Box
               sx={{
-                borderRadius: "10px",
+                width: "54vw",
+                height: "54vh",
+                borderRadius: "20px",
                 overflow: "hidden",
               }}
             >
@@ -52,39 +102,77 @@ const VideoDetail: React.FC = () => {
                 url={`https://www.youtube.com/watch?v=${id}`}
                 className="react-player"
                 controls
-                width="100%"
-                height="auto"
               />
             </Box>
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-              {title}
-            </Typography>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{ color: "#fff" }}
-              py={1}
-              px={2}
+            <Box
+              sx={{
+                width: "54vw",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+              }}
             >
-              <Link to={`/channel/${channelId}`}>
-                <Typography variant="subtitle1" color="#fff">
-                  {channelTitle}
-                </Typography>
-              </Link>
-            </Stack>
+              <Typography
+                color="#000"
+                fontSize="22px"
+                fontWeight="bold"
+                marginTop="10px"
+              >
+                {title || "Loading..."}
+              </Typography>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={channelAvatar}
+                  alt={channelTitle}
+                  width={50}
+                  height={50}
+                  style={{ borderRadius: "50%" }}
+                />
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginLeft: "5px",
+                  }}
+                >
+                  <Link to={`/channel/${channelId}`}>
+                    <Typography
+                      fontSize="16px"
+                      fontWeight="bold"
+                      color="#000"
+                      sx={{ marginLeft: "10px" }}
+                    >
+                      {channelTitle}
+                    </Typography>
+                  </Link>
+                  <Typography
+                    fontSize="14px"
+                    color="#7F7F7F"
+                    sx={{ marginLeft: "10px" }}
+                  >
+                    • {subscriberCount} 订阅者
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
           </Box>
+          <Box
+            sx={{
+              flex: 1,
+              padding: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          ></Box>
         </Box>
-        <Box
-          px={2}
-          py={{ md: 1, xs: 5 }}
-          height="92vh"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ width: "320px", marginLeft: "auto" }}
-        >
-          <Loader />
-        </Box>
-      </Stack>
+      </Box>
     </Box>
   );
 };
