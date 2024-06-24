d9d85b55030e08a3632f1f4b2cb4fd4fe2fe0682
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-06-24 20:16:33 +0800

    Create list1.tsx

---
+++ src/video/components/video/list1.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./card1";
+import { VideoTypes } from "./types";
+
+const VideoList: React.FC<VideoListProps> = ({
+  videoList,
+  direction,
+  maxCards,
+}) => {
+  const slicedVideoList = maxCards ? videoList.slice(0, maxCards) : videoList;
+
+  return (
+    <Stack direction={direction || "column"} flexWrap="wrap" marginTop="10px">
+      {slicedVideoList.map((video, index) => (
+        <Box key={index}>{video.id.videoId && <VideoCard video={video} />}</Box>
+      ))}
+    </Stack>
+  );
+};
+
+interface VideoListProps {
+  videoList: VideoTypes[];
+  direction?: "row" | "column";
+  maxCards?: number;
+}
+
+export default VideoList;
